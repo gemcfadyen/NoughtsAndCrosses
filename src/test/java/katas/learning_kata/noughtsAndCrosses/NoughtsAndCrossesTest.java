01692ee7238fe52308fdf6b3f9cf971803963f68
gemcfadyen <gemcfadyen@hotmail.co.uk>
2013-09-08 21:26:15 +0100

    removing unused import

--- src/test/java/katas/learning_kata/noughtsAndCrosses/NoughtsAndCrossesTest.java
+++ src/test/java/katas/learning_kata/noughtsAndCrosses/NoughtsAndCrossesTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
